5c0fbc1ebb020861682574b925a59de019dd93e5
Roshan Poudyal <roshanpoudyal@users.noreply.github.com>
2017-05-28 23:58:51 +0200

    working copy of game ready
    to do : 1.) feedback when game is over 2.) try some animated effects as feedback. 3.) restart game when it touches enemy or falls from the ground

---
+++ Assets/scripts/guardBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guardBehavior : MonoBehaviour {
+	private Vector3 guardStartPosition = new Vector3(-1.25f,-0.25f,2.5f);
+	private Vector3 guardEndPosition = new Vector3(-1.25f,-0.25f,-2.5f);
+	private float patrolSpeed = 0.5f;
+
+	// Update is called once per frame
+	void Update () {
+		// make a patrolling movement with the guard
+		transform.position = 
+			Vector3.Lerp ( guardStartPosition, guardEndPosition,
+						   Mathf.PingPong(Time.time*patrolSpeed, 1.0f ));
+
+	}
+}
---
+++ Assets/scripts/playerbehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerbehaviour : MonoBehaviour {
+	private float moveSpeed = 50f;
+	private float moveMaxSpeed = 53f;
+	private Vector3 playerMovement;
+	private Rigidbody playercube;
+
+	// this is called when the game is initialized
+	void Start (){
+		playercube = GetComponent<Rigidbody> ();
+	}
+
+	void OnTriggerEnter(Collider allTriggers){
+		Destroy (allTriggers.gameObject);
+		print ("Game over!");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+
+		playerMovement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+
+		// the condition below for the playercube speed makes the pickup faster 
+		// with the speed (snap and catches up with speed)
+		if (playercube.velocity.magnitude < moveMaxSpeed) {
+			playercube.AddForce (playerMovement * moveSpeed);
+		}
+	}
+}
