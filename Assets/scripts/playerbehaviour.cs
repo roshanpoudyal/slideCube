a173fa3767020e2c2dc616848dee387a002224ea
Roshan Poudyal <roshanpoudyal@users.noreply.github.com>
2017-05-31 20:32:09 +0200

    player stops when reaching the destination and the game restarts
    player restarts position once it reaches the destination. Player freeze when in destination and can move once restarted. Logic for pausing the player needs and optimization.

--- Assets/scripts/playerbehaviour.cs
+++ Assets/scripts/playerbehaviour.cs
@@ -8,14 +8,34 @@ public class playerbehaviour : MonoBehaviour {
 	private float moveMaxSpeed = 53f;
 	private Vector3 playerMovement;
 	private Rigidbody playercube;
+	private Vector3 playerStartPosition;
 
 	// this is called when the game is initialized
 	void Start (){
 		playercube = GetComponent<Rigidbody> ();
+
+		playerStartPosition = playercube.transform.position;
 	}
 
 	void OnTriggerEnter(Collider allTriggers){
-		Destroy (allTriggers.gameObject);
+		StartCoroutine(pauseGame());
+	}
+
+	// this Coroutine is called by from ontriggerenter
+	// to make sure the player is repositioned when 
+	IEnumerator pauseGame() {
+		//make the playercube stop
+		moveSpeed = 0f;
+
+		//wait for some time
+		yield return new WaitForSeconds(5);
+
+		// reset the playercube to starting position
+		playercube.transform.position = playerStartPosition;
+
+		// make the player able to move on getting keyboard input
+		// find a better way to make the player stop this is a workaround
+		moveSpeed = 50f;
 	}
 
 	// Update is called once per frame
